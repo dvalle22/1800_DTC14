72d1a84b16854776df73d2e58f616d7f08f7a608
LeoLi225 <cli256@my.bcit.ca>
2021-11-02 17:34:25 -0700

    Use FireBase to make login authentication.

--- scripts/my_script.js
+++ scripts/my_script.js
@@ -18,6 +18,17 @@ let navbar_setter = function() {
 document.body.onload = navbar_setter;
 /*********************************************/
 
+var firebaseConfig = {
+   apiKey: "AIzaSyC-XdlrUduNApNzif0Rmcm9u4xMdnfJziQ",
+   authDomain: "ucare-74097.firebaseapp.com",
+   projectId: "ucare-74097",
+   storageBucket: "ucare-74097.appspot.com",
+   messagingSenderId: "965844702573",
+   appId: "1:965844702573:web:797a872a82ec69d3353664"
+ };
+
+
+ const app = firebase.initializeApp(firebaseConfig);
 
 function sayHello() {
    firebase.auth().onAuthStateChanged(function (user) {
