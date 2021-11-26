056117a546575c40ac83dd8b2c417b62566f34ea
LeoLi225 <cli256@my.bcit.ca>
2021-11-26 15:40:43 -0800

    Ad news.css

--- scripts/my_script.js
+++ scripts/my_script.js
@@ -121,7 +121,7 @@ function goBack() {
             }
             else
                alert("No page to go back to.")
-            
+
          } else {
             console.log("No such document!");
          }
@@ -143,3 +143,81 @@ function myFunction() {
       x.style.display = "block";
    }
 }
+
+
+function insertTitle() {
+   firebase.auth().onAuthStateChanged(user => {
+
+      if (user) {
+
+
+         currentUser = db.collection("users").doc(user.uid).collection("saved_news").doc("0001");
+
+         currentUser.get()
+            .then(saved_newsDoc => {
+               var Title = saved_newsDoc.data().title;
+               var url = saved_newsDoc.data().url;
+               console.log(Title);
+               $("#title1").text(Title);
+               $("#url1").text(url);
+            })
+            currentUser = db.collection("users").doc(user.uid).collection("saved_news").doc("0002");
+
+         currentUser.get()
+            .then(saved_newsDoc => {
+               var Title = saved_newsDoc.data().title;
+               var url = saved_newsDoc.data().url;
+               console.log(Title);
+               $("#title2").text(Title);
+               $("#url2").text(url);
+            })
+            currentUser = db.collection("users").doc(user.uid).collection("saved_news").doc("0003");
+
+         currentUser.get()
+            .then(saved_newsDoc => {
+               var Title = saved_newsDoc.data().title;
+               var url = saved_newsDoc.data().url;
+               console.log(Title);
+               $("#title3").text(Title);
+               $("#url3").text(url);
+            })
+            currentUser = db.collection("users").doc(user.uid).collection("saved_news").doc("0004");
+
+         currentUser.get()
+            .then(saved_newsDoc => {
+               var Title = saved_newsDoc.data().title;
+               var url = saved_newsDoc.data().url;
+               console.log(Title);
+               $("#title4").text(Title);
+               $("#url4").text(url);
+            })
+            currentUser = db.collection("users").doc(user.uid).collection("saved_news").doc("0005");
+
+         currentUser.get()
+            .then(saved_newsDoc => {
+               var Title = saved_newsDoc.data().title;
+               var url = saved_newsDoc.data().url;
+               console.log(Title);
+               $("#title5").text(Title);
+               $("#url5").text(url);
+            })
+            currentUser = db.collection("users").doc(user.uid).collection("saved_news").doc("0006");
+
+         currentUser.get()
+            .then(saved_newsDoc => {
+               var Title = saved_newsDoc.data().title;
+               var url = saved_newsDoc.data().url;
+               console.log(Title);
+               $("#title6").text(Title);
+               $("#url6").text(url);
+            })
+
+      } else {
+         // No user is signed in.
+      }
+   });
+}
+insertTitle();
+
+
+
