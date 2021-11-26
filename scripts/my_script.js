3deae6dda041c5b946a37ade2b80f8e1899a8392
Melanie Ta <nngoc2701@gmail.com>
2021-11-26 01:34:28 -0800

    Tidy up work tree

--- scripts/my_script.js
+++ scripts/my_script.js
@@ -9,7 +9,7 @@ let navbar_setter = function () {
    document.body.insertBefore(nav_container, document.body.firstChild);
 
    let xHR_nav = new XMLHttpRequest();
-   xHR_nav.open("GET", "./scripts/navbar_html.xml");
+   xHR_nav.open("GET", "./navbar_html.xml");
    xHR_nav.onreadystatechange = function () {
       nav_container.innerHTML = xHR_nav.responseText;
    };
